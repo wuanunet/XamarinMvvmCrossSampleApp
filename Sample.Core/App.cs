26721861f557a2e73c2d59288ec9e9143cbfc1f5
Cheng-Ju Wu <wuanunet@hotmail.com>
2016-06-08 14:11:12 +0800

    加入專案檔案。

---
+++ Sample.Core/App.cs
@@ -0,0 +1,17 @@
+using MvvmCross.Platform.IoC;
+
+namespace XamarinMvvmCrossSampleApp
+{
+    public class App : MvvmCross.Core.ViewModels.MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<ViewModels.FirstViewModel>();
+        }
+    }
+}
