4688bd3d1a43e40d54a3d6739da3af31046c2a33
Cheng-Ju Wu <wuanunet@hotmail.com>
2016-06-08 14:52:54 +0800

    Add New Project For Sample.UWP and reference MvvmCross StarterPack tool. (UWP Project)

---
+++ Sample.UWP/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Windows.UI.Xaml.Controls;
+
+//空白頁項目範本收錄在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Sample.UWP
+{
+    /// <summary>
+    /// 可以在本身使用或巡覽至框架內的空白頁面。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
